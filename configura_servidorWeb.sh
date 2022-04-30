a80fb5e9b8fce8cba6b34997e0dbda5c7a6bc8c3
MaxwellSilveira <88736324+MaxwellSilveira@users.noreply.github.com>
2022-04-30 17:21:57 -0300

    Update configura_servidorWeb.sh

--- configura_servidorWeb.sh
+++ configura_servidorWeb.sh
@@ -8,16 +8,24 @@ apt-get install apache2
 
 apt-get install wget
 
-wget https://gist.github.com/nunomazer/873219/archive/690e1f604dc8ead2583589e1aee6f8a42040a035.zip
-
 apt-get install zip
 
 apt-get install unzip
 
+wget https://gist.github.com/nunomazer/873219/archive/690e1f604dc8ead2583589e1aee6f8a42040a035.zip
+
 unzip 873219-690e1f604dc8ead2583589e1aee6f8a42040a035
+
+cd 873219-690e1f604dc8ead2583589e1aee6f8a42040a035
+
+cp cv-resumido.html /var/www/html/
+
+cp style.css /var/www/html/
+
+rm index.html
 
 cd /var/www/html/
 
-rm index.html
+mv cv-resumido.html index.html
 
 /etc/init.d/apache2 start
