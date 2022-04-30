a44bddaacaa9cc26a67d0f3ba14a24b2a9dee21c
MaxwellSilveira <88736324+MaxwellSilveira@users.noreply.github.com>
2022-04-30 15:02:44 -0300

    Create configura_servidorWeb.sh

---
+++ configura_servidorWeb.sh
@@ -0,0 +1,23 @@
+apt-get install
+
+apt-get update
+
+apt-get upgrade
+
+apt-get install apache2
+
+apt-get install wget
+
+wget https://gist.github.com/nunomazer/873219/archive/690e1f604dc8ead2583589e1aee6f8a42040a035.zip
+
+apt-get install zip
+
+apt-get install unzip
+
+unzip 873219-690e1f604dc8ead2583589e1aee6f8a42040a035
+
+cd /var/www/html/
+
+rm index.html
+
+/etc/init.d/apache2 start
